f96ddab7581c512993b532ca38cb4749d9e2da61
nicklasstellberg <nicklas.stellberg@hotmail.com>
2025-02-20 16:26:24 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiWithMudBlazor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
